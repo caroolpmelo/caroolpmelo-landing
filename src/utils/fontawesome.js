9cbfdc4dc86f681156c326d1d8e669a9f3cc1a38
Carolina Melo <caroolpmelo@gmail.com>
2021-05-18 10:56:05 -0300

    :constrution: WIP
    Signed-off-by: Carolina Melo <caroolpmelo@gmail.com>

---
+++ src/utils/fontawesome.js
@@ -0,0 +1,6 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCoffee)
+/* Now add the faCoffee icon to our library and we can use
+the string name 'coffee' in our app */
