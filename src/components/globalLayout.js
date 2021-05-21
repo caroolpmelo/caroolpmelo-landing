e4ad2a6060760a0f8b6381283bbc961dd0ef8a30
Carolina Melo <caroolpmelo@gmail.com>
2021-05-20 23:52:23 -0300

    :sparkles: Adds functional FA icons and adjusts global style (WIP)
    Signed-off-by: Carolina Melo <caroolpmelo@gmail.com>

---
+++ src/components/globalLayout.js
@@ -0,0 +1,4 @@
+import * as React from "react";
+import "./globalLayout.css";
+
+export default ({ children }) => <div>{children}</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,129 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as React from "react";
 import avatar from '../images/icon.png';
-import "../utils/fontawesome";
+import styled from "styled-components";
+import GlobalLayout from "../components/globalLayout"
+
+import { FaAt } from "@react-icons/all-files/fa/FaAt";
+import { FaCodepen } from "@react-icons/all-files/fa/FaCodepen";
+import { FaFacebook } from "@react-icons/all-files/fa/FaFacebook";
+import { FaFile } from "@react-icons/all-files/fa/FaFile";
+import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
+import { FaItchIo } from "@react-icons/all-files/fa/FaItchIo";
+import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 
 // styles
-const pageStyles = {
-  backgroundColor: "#0A0C1B",
-  fontFamily: "Verdana"
-}
-const cardStyles = {
-  backgroundColor: "#1C1938",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between"
-}
-const titleStyles = {
-  color: "#A95FD4"
-}
-const descriptionStyles = {
-  color: "#FFFFFF"
-}
-const linksStyles = {
-  alignContent: "center",
-  display: "flex",
-  justifyContent: "space-between",
-  listStyleType: "none",
-  padding: "0"
-}
+const MainContainer = styled.main`
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  background-color: #0A0C1B;
+  font-family: Verdana;
+`;
+const MainCard = styled.div`
+  background-color: #1C1938;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const MainTitle = styled.h1`
+  color: #A95FD4;
+`;
+const MainDescription = styled.div`
+  color: #FFFFFF;
+`;
+const UnorderedList = styled.ul`
+  align-content: center;
+  display: flex;
+  justify-content: space-between;
+  list-style-type: none;
+  padding: 0;
+`;
+const ListItem = styled.li`
+  color: white;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+const LinkItem = styled.a`
+  color: white;
+  text-decoration: none;
+`;
+const AvatarImg = styled.img`
+  max-width: 484px;
+  max-height: 396px;
+`;
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <MainContainer>
       <title>Carolina Melo - @caroolpmelo</title>
-      <div style={cardStyles}>
+      <MainCard>
         <div>
-          <h1 style={titleStyles}>Carolina Melo</h1>
-          <div style={descriptionStyles}>
+          <MainTitle>Carolina Melo</MainTitle>
+          <MainDescription>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          </div>
+          </MainDescription>
           <div>
-            <ul style={linksStyles}>
-              <li>Linkedin</li>
-              <li><i class="fab fa-github-square"></i></li>
-              <li>Insta</li>
-              <li>
-                <i alt="CodePen" title="CodePen" class="fab fa-codepen"></i>
-                <FontAwesomeIcon icon={'coffee'}/>
-              </li>
-            </ul>
+            <UnorderedList>
+              <ListItem class="dropdown">
+                <div id="cvDropdown" class="dropdown-content">
+                  {/* <a
+                    href="https://drive.google.com/file/d/1FPCYCr_ppOEdyr9VxJ5dUmqwjvQLsFpv/view?usp=sharing"
+                    target="_blank"
+                  >
+                    Português (pt-br)
+                  </a>
+                  <a
+                    href="https://drive.google.com/file/d/1vxdsUUfkUvDwoSVMV0ztJlaMUGdBpde7/view?usp=sharing"
+                    target="_blank"
+                    >English (en-us)</a
+                  > */}
+                </div>
+                {/* <i alt="Resume" title="Resume" class="far fa-file dropbtn" onclick="openCvMenu()"></i> */}
+                <FaFile alt="Resume" title="Resume"></FaFile>
+              </ListItem>
+              <ListItem>
+                <LinkItem href="https://github.com/caroolpmelo" target="_blank">
+                  <FaGithub alt="GitHub" title="GitHub"></FaGithub>
+                </LinkItem>
+              </ListItem>
+              <ListItem>
+                <LinkItem href="https://codepen.io/caroolpmelo" target="_blank">
+                  <FaCodepen alt="CodePen" title="CodePen"></FaCodepen>
+                </LinkItem>
+              </ListItem>
+              <ListItem>
+                <LinkItem href="https://caroolpmelo.itch.io" target="_blank">
+                  <FaItchIo alt="itch.io" title="itch.io"></FaItchIo>
+                </LinkItem>
+              </ListItem>
+              <ListItem>
+                <LinkItem href="https://linkedin.com/in/cmpmelo" target="_blank">
+                  <FaLinkedin alt="LinkedIn" title="LinkedIn"></FaLinkedin>
+                </LinkItem>
+              </ListItem>
+              <ListItem>
+                <LinkItem href="https://fb.com/caroolpmelo" target="_blank">
+                  <FaFacebook alt="Facebook" title="Facebook"></FaFacebook>
+                </LinkItem>
+              </ListItem>
+              <ListItem>
+                <LinkItem href="mailto:caroolpmelo@gmail.com">
+                  <FaAt alt="Contact me : )" title="Contact me : )"></FaAt>
+                </LinkItem>
+              </ListItem>
+            </UnorderedList>
           </div>
         </div>
         <div>
-          <img src={avatar} alt="Myself :)" />
+          <AvatarImg src={avatar} alt="Myself :)" />
         </div>
-      </div>
-    </main>
+      </MainCard>
+    </MainContainer>
   )
 }
 
-export default IndexPage
+// export default IndexPage
+export default () => <GlobalLayout><IndexPage /></GlobalLayout>
